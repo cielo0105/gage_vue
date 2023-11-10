b63bb3979cb0e16f543d755aac05aaf4ef5137c8
qlenfrl101 <qlenfrl101@naver.com>
2023-11-10 17:58:47 +0900

    :sparkles: navigation 추가

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -7,7 +7,6 @@ onMounted(() => {
     initMap()
   } else {
     const script = document.createElement('script')
-    /* global kakao */
     script.onload = () => window.kakao.maps.load(initMap)
     script.src =
       '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=8980521e66956686ad980618b70271ab'
@@ -16,13 +15,22 @@ onMounted(() => {
 })
 
 const initMap = () => {
-  var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-    mapOption = {
-      center: new window.kakao.maps.LatLng(37.564343, 126.947613), // 지도의 중심좌표
-      level: 3 // 지도의 확대 레벨
-    }
+  let mapContainer = document.getElementById('map') // 지도를 표시할 div
+  let mapOption = {
+    center: new window.kakao.maps.LatLng(37.564343, 126.947613), // 지도의 중심좌표
+    level: 3 // 지도의 확대 레벨
+  }
 
-  var map = new window.kakao.maps.Map(mapContainer, mapOption)
+  let map = new window.kakao.maps.Map(mapContainer, mapOption)
+
+  window.navigator.geolocation.getCurrentPosition((p) => {
+    let lat = p.coords.latitude
+    let lng = p.coords.longitude
+    if (!lat && !lng) {
+      console.log('lat==', lat, ', lng==', lng)
+      map.setCenter(new window.kakao.maps.LatLng(lat, lng))
+    }
+  })
 }
 </script>
 
