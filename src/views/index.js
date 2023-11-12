a124bec0c551c7fdbb2a7d9cfe9916fc5d2869d3
cielo0105 <cielo0105@naver.com>
2023-11-13 01:13:02 +0900

    :sparkles: 공지사항 list, write

--- src/views/index.js
+++ src/views/index.js
@@ -1,17 +1,9 @@
-import HomeView from '@/views/HomeView.vue';
-import JoinView from '@/views/JoinView.vue';
-import LoginView from '@/views/LoginView.vue';
-import MapView from '@/views/MapView.vue';
-import NoticeView from '@/views/NoticeView.vue';
-import ReportView from '@/views/ReportView.vue';
-import NewsView from '@/views/NewsView.vue';
+import HomeView from '@/views/HomeView.vue'
+import JoinView from '@/views/JoinView.vue'
+import LoginView from '@/views/LoginView.vue'
+import MapView from '@/views/MapView.vue'
+import NoticeView from '@/views/NoticeView.vue'
+import ReportView from '@/views/ReportView.vue'
+import NewsView from '@/views/NewsView.vue'
 
-export {
-    HomeView,
-    JoinView,
-    LoginView,
-    MapView,
-    NoticeView,
-    ReportView,
-    NewsView
-}
+export { HomeView, JoinView, LoginView, MapView, NoticeView, ReportView, NewsView }
