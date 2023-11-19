a59303fdaecef0d8226a195e5d0d1ab0174fb928
최지우 <qlenfrl101@naver.com>
2023-11-20 03:03:47 +0900

    :sparkles: 회원가입에서 이메일 인증 받기

--- src/components/api/memberApi.js
+++ src/components/api/memberApi.js
@@ -5,11 +5,15 @@ const local = localAxios()
 const url = '/member'
 
 const registMember = (member, success, fail) => {
-    local.post(`${url}/regist`, JSON.stringify(member)).then(success).catch(fail)
+  local.post(`${url}/regist`, JSON.stringify(member)).then(success).catch(fail)
 }
 
 const loginMember = (member, success, fail) => {
-    local.post(`${url}/login`, JSON.stringify(member)).then(success).catch(fail)
+  local.post(`${url}/login`, JSON.stringify(member)).then(success).catch(fail)
 }
 
-export {registMember, loginMember}
+const sendAuthEmailNum = (email, success, fail) => {
+  local.post(`/send-mail/email`, JSON.stringify(email)).then(success).catch(fail)
+}
+
+export { registMember, loginMember, sendAuthEmailNum }
---
+++ src/components/auth/InputBoxWithBtn.vue
@@ -0,0 +1,73 @@
+<script setup>
+const props = defineProps({
+  modelValue: String,
+  width: String,
+  height: String,
+  type: String,
+  label: String,
+  btnName: String
+})
+const emit = defineEmits(['update:modelValue'])
+const handleInput = ($event) => {
+  emit('update:modelValue', $event.target.value)
+}
+</script>
+
+<template>
+  <div :style="{ width: props.width }">
+    <label class="text">{{ label }}</label>
+    <input
+      class="input-box"
+      :value="modelValue"
+      @input="handleInput"
+      :type="props.type"
+      :style="{ height: props.height }"
+    />
+    <button class="input-btn" :style="{ height: props.height }" @click="$emit('click-btn')">
+      {{ btnName }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.input-box {
+  border-radius: 0.625rem;
+  background: #fff;
+  border: none;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
+  margin-bottom: 1.87rem;
+  margin-right: 5%;
+  outline: none;
+  padding: 0.25rem 1rem 0.1rem 1rem;
+  font-size: 1.25rem;
+  width: 70%;
+}
+
+.input-btn {
+  width: 25%;
+  border-radius: 0.625rem;
+  background: #fff;
+  border: none;
+  margin: 0.5rem 0;
+
+  color: #6e2add;
+  text-align: center;
+  line-height: normal;
+  font-size: 1.25rem;
+  font-weight: 700;
+}
+
+.input-btn:hover {
+  background: #6e2add;
+  color: #fff;
+}
+
+.text {
+  color: #6e2add;
+  font-size: 1.25rem;
+  font-weight: 700;
+  display: block;
+  padding-left: 0.5rem;
+  padding-bottom: 0.5rem;
+}
+</style>
